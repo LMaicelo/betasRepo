3ce1ebc01f8ec4ddecf4832c77e4f1cb6d326d81
Leonardo Maicelo <leonardomaicelo@gmail.com>
2020-09-29 19:10:22 -0400

    Fixed server

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const app = express();
 
 const brotherRoutes = require('./api/routes/brothers');
 const lineRoutes = require('./api/routes/lines');
-const indexRoutes = require('./api/routes/index');
 
 app.use('/lines', lineRoutes);
 app.use('/brothers', brotherRoutes);
-app.use('/', indexRoutes);
 
 module.exports = app;
